86fba704a12b0e2af587f91af328651c73506f6e
Nerah <alascenci@gmail.com>
2020-09-28 21:15:58 +0200

    double clic sur zone => génère dynamiquement des cartes

--- src/containers/Zone/index.js
+++ src/containers/Zone/index.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
 import reactable from 'reactablejs';
+import { v4 as uuidv4 } from 'uuid';
 import Card from "../../components/Card";
 
 const ZoneWrapper = styled.div`
@@ -18,17 +19,25 @@ function StaticZone({children, ...props}) {
 
 const ZoneReactable = reactable(StaticZone);
 
+const INITIAL_CARDS = [];
 const DEFAULT_NAME = "DEFAULT_NAME";
 const DEFAULT_DESCRIPTION = "DEFAULT_DESCRIPTION";
 
 export default function Zone() {
+  const [cards, setCards] = useState(INITIAL_CARDS)
+
+  const addCard = () => {
+    setCards(oldCards =>
+        [...oldCards,
+          <Card key={uuidv4()}
+                name={DEFAULT_NAME}
+                description={DEFAULT_DESCRIPTION}
+          />])
+  }
+
   return (
-      <ZoneReactable
-          onDoubleTap={() => {
-            console.log("create new card!")
-          }}
-      >
-        <Card name={DEFAULT_NAME} description={DEFAULT_DESCRIPTION}/>
+      <ZoneReactable onDoubleTap={() => addCard()}>
+        {cards.map(card => card)}
       </ZoneReactable>
   )
 }
