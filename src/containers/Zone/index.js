ef7df1e6e386f216eb7decdaa0e8daf07480d290
Nerah <alascenci@gmail.com>
2020-09-28 20:32:42 +0200

    zone blanche doucle-clic enclenche un event

--- src/containers/Zone/index.js
+++ src/containers/Zone/index.js
@@ -1,15 +1,29 @@
 import React from "react";
 import styled from "styled-components";
+import reactable from 'reactablejs';
 
 const ZoneWrapper = styled.div`
   width: 100%;
   height: 75vh;
 `;
 
-export default function Zone({children}) {
+function StaticZone({children, ...props}) {
   return (
-      <ZoneWrapper>
+      <ZoneWrapper ref={props.getRef}>
         {children}
       </ZoneWrapper>
   );
+}
+
+const ZoneReactable = reactable(StaticZone);
+
+export default function Zone({children}) {
+  return (
+      <ZoneReactable
+          onDoubleTap={() => {
+            console.log("create new card!")
+          }}
+          children={children}
+      />
+  )
 }
